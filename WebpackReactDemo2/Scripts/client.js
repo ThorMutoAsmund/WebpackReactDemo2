c5e257f5cea6f9b8e33eb3deb2a173872f00711b
Thor Muto Asmund <thorasmund@gmail.com>
2017-03-02 00:45:02 +0100

    First checkin
    Contains react, babel, webpack, npm init

---
+++ WebpackReactDemo2/Scripts/client.js
@@ -0,0 +1,24 @@
+﻿//require("./module1.js");
+//require("./module2.js");
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>It works really well! I think so</h3>
+                <table>
+                <tbody>
+                <tr><td>HEY</td></tr>
+                </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<Layout />, app);
---
+++ WebpackReactDemo2/Scripts/module2.js
@@ -0,0 +1,6 @@
+﻿//var _ = require("lodash");
+//var $ = require("jquery")
+
+//_.forEach(['Thor','Rolf'], function (a) {
+//    $("#content").append("<p>"+a+"</p>");
+//});
